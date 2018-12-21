0b007328315f4f38488baa88d59aad6395d09c3e
Ravishankar Ahirwar <ravishankar.ahirwar@gmail.com>
2018-12-21 18:37:39 +0530

    Notification added for Android 8, update latest version of all library, ready for new release.

--- app/src/main/java/world/best/musicplayer/adapters/FooterViewHolder.java
+++ app/src/main/java/world/best/musicplayer/adapters/FooterViewHolder.java
@@ -4,23 +4,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 import world.best.musicplayer.R;
 
 public class FooterViewHolder extends RecyclerView.ViewHolder {
 
     public TextView footerText;
-    private AdView mAdView;
 
     public FooterViewHolder (View itemView) {
         super(itemView);
 
-        mAdView = (AdView)itemView.findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
-        footerText = (TextView) itemView.findViewById(R.id.footer_text);
+        footerText = itemView.findViewById(R.id.footer_text);
     }
 }
